a5c3407db5a6a8273401c9ed618430cc1160c556
bunnysamurai <pohljedi@gmail.com>
2023-12-15 22:22:24 -0500

    more tinkering, probably too complicated

--- app/firmware.cpp
+++ app/firmware.cpp
@@ -5,68 +5,92 @@
 #include <bit>
 #include <cstddef>
 #include <cstdint>
+#include <limits>
 
-static constexpr auto NEOPIXEL_PIN{ 2 };
-static constexpr auto NEOPIXEL_LED_COUNT{ 24 };
+static constexpr auto NEOPIXEL_PIN{2};
+static constexpr auto NEOPIXEL_LED_COUNT{24};
 
-static const PIO PIO_INDEX{ pio0 };
-static constexpr auto PIO_STATE_MACHINE{ 0 };
+static const PIO PIO_INDEX{pio0};
+static constexpr auto PIO_STATE_MACHINE{0};
 
-template< size_t N >
-    requires( std::popcount( N ) == 1 )
+// FIXME THIS IS TERRIBLE, AS WE CAN RUN INTO NUMERICAL ISSUES VERY QUICKLY
+inline constexpr double integral_pow(double value, uint exp)
+{
+    double rv{1};
+    for (uint ii{0}; ii < exp; ++ii)
+    {
+        rv *= value;
+    }
+    return rv;
+}
+static_assert(integral_pow(2.0, 4) == 2.0 * 2.0 * 2.0 * 2.0);
+
+inline constexpr uint64_t factorial(uint64_t value)
+{
+    uint64_t rv{1};
+    for (uint64_t ii = 0; ii < value; ++ii)
+    {
+        rv *= ii + 1;
+    }
+    return rv;
+}
+static_assert(factorial(1) == 1);
+static_assert(factorial(2) == 2);
+static_assert(factorial(3) == 1 * 2 * 3);
+static_assert(factorial(5) == 120);
+
+inline constexpr double sine_power_series(double radian, size_t terms)
+{
+    double rv{};
+    for (size_t n = 0; n < terms; ++n)
+    {
+        const double numer{integral_pow(radian, 2 * n + 1)};
+        const double denom{static_cast<double>(factorial(2 * n + 1))};
+        const double factor{(n & 0x1U) == 0 ? 1.0 : -1.0};
+        rv += (numer / denom) * factor;
+    }
+    return rv;
+}
+static_assert(sine_power_series(0.0, 5) == 0);
+static_assert(sine_power_series(3.141592653589793, 5) > 0);
+static_assert(sine_power_series(270 * 3.141592653589793 / 180, 5) < 0);
+static_assert(sine_power_series(2 * 3.141592653589793, 5) > 0);
+
+template <size_t N>
+    requires(std::popcount(N) == 1)
 static constexpr auto SINE_TABLE{
-    []() {
-        // FIXME THIS IS TERRIBLE, AS WE CAN RUN INTO NUMERICAL ISSUES VERY QUICKLY
-        auto&& integral_pow{ []( double value, int exp ) {
-            double rv{ 1 };
-            for( int ii{ 0 }; ii < exp; ++ii )
-            {
-                rv *= value;
-            }
-            return rv;
-        } };
-        auto&& factoral{ []( int value ) {
-            double rv{ 1 };
-            for( int ii = 0; ii < value; ++ii )
-            {
-                rv *= ii + 1;
-            }
-            return rv;
-        } };
-
-        auto&& power_series{ [ = ]( double radian, size_t terms ) {
-            double rv{};
-            for( size_t n = 0; n < terms; ++n )
-            {
-                const double numer{ integral_pow( radian, 2 * n + 1 ) };
-                const double denom{ factoral( 2 * n + 1 ) };
-                const double factor{ ( n & 0x1U ) == 0 ? 1.0 : -1.0 };
-                rv += numer / denom * factor;
-            }
-            return rv;
-        } };
+    []()
+    {
+        constexpr auto POWER_SERIES_TERMS{5};
         // https://math.stackexchange.com/questions/2853310/how-to-calculate-the-sine-manually-without-any-rules-calculator-or-anything-el#2853320
         // using the power-series method
-        std::array< uint8_t, N > rv{};
-        for( size_t ii = 0; ii < std::size( rv ); ++ii )
+        std::array<uint8_t, N> rv{};
+        for (size_t ii = 0; ii < std::size(rv); ++ii)
         {
-            const double radian{ ( 2 * 3.141592653589793 ) / std::size( rv ) * ii };
-            rv[ ii ] = power_series( radian, 1 ) * 128 + 127;
+            const double radian{static_cast<double>((2 * 3.141592653589793) / std::size(rv) * ii)};
+            const auto value{sine_power_series(radian, POWER_SERIES_TERMS)};
+            rv[ii] = static_cast<uint8_t>((value + 1) / 2) + std::numeric_limits<uint8_t>::max();
         }
         return rv;
-    }()
-};
+    }()};
 
-inline constexpr auto SINE_TABLE_MASK( const auto& sine_table ) { return static_cast< uint8_t >( ~( std::size( sine_table ) - 1 ) ); }
+inline constexpr auto SINE_TABLE_MASK(const auto &sine_table) { return static_cast<uint8_t>(~(std::size(sine_table) - 1)); }
 
 int main()
 {
-    pico_ws2812::WRGB_String_Driver drv( PIO_INDEX, PIO_STATE_MACHINE, NEOPIXEL_PIN, NEOPIXEL_LED_COUNT );
+    pico_ws2812::PIO_NeoPixel_Driver driver(PIO_INDEX, PIO_STATE_MACHINE, NEOPIXEL_PIN);
+    pico_ws2812::WRGB_Driver pixel_driver{driver};
+    pico_ws2812::WRGB_Pattern_Driver pattern_driver{pixel_driver, NEOPIXEL_LED_COUNT};
 
-    uint8_t index{ 0 };
-    for( ;; )
+    auto pattern_generator{[index = uint{0}](uint) mutable
+                           {
+                               const auto pixel_value{SINE_TABLE<1024>[index++ & SINE_TABLE_MASK(SINE_TABLE<1024>)]};
+                               return pico_ws2812::WRGB{.white{pixel_value}, .red{0}, .green{0}, .blue{0}};
+                           }};
+
+    for (;;)
     {
-        drv.put_pixel( SINE_TABLE< 128 >[ index++ & SINE_TABLE_MASK( SINE_TABLE< 128 > ) ] );
-        sleep_ms( 10 );
+        pattern_driver.put_pattern(pattern_generator);
+        sleep_ms(5);
     }
 }
--- ws2812/ws2812.hpp
+++ ws2812/ws2812.hpp
@@ -1,4 +1,4 @@
-#if !defined( WS2812_HPP )
+#if !defined(WS2812_HPP)
 #define WS2812_HPP
 
 /**
@@ -17,79 +17,158 @@
 
 namespace pico_ws2812
 {
-
-    class RGB_String_Driver
-    {
-    public:
-        using Callable_T = uint32_t ( * )( uint pixel_index );
-
-        explicit RGB_String_Driver( PIO pio_index, int state_machine_index, int gpio_pin, size_t number_of_pixels ) noexcept :
-            m_pio{ pio_index }, m_state_machine{ state_machine_index }, m_number_of_pixels{ number_of_pixels }
-        {
-            uint offset = pio_add_program( m_pio, &ws2812_program );
-            ws2812_program_init( m_pio, m_state_machine, offset, gpio_pin, 800000, false );
-        }
-
-        void put_pattern( Callable_T functor )
-        {
-            for( uint ii = 0; ii < m_number_of_pixels; ++ii )
-            {
-                put_pixel( functor( ii ) );
-            }
-        }
-
-        void put_pixel( uint8_t r, uint8_t g, uint8_t b )
-        {
-            pio_sm_put_blocking( m_pio, m_state_machine, rgb_u32( r, g, b ) );
-        }
-
-    protected:
-        PIO m_pio;
-        int m_state_machine;
-        size_t m_number_of_pixels;
-
-        void put_pixel( uint32_t pixel_grb )
-        {
-            pio_sm_put_blocking( m_pio, m_state_machine, pixel_grb );
-        }
-
-        inline uint32_t rgb_u32( uint8_t r, uint8_t g, uint8_t b )
-        {
-            return ( static_cast< uint32_t >( r ) << 16 ) |
-                   ( static_cast< uint32_t >( g ) << 24 ) |
-                   ( static_cast< uint32_t >( b ) << 8 );
-        }
+    template <class T>
+    concept string_interface = requires(T a, uint32_t pixel) {
+        a.put_pixel(pixel);
+        a.set_rgb_mode();
+        a.set_wrgb_mode();
     };
 
-    class WRGB_String_Driver : public RGB_String_Driver
+    struct RGB
+    {
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+    };
+    template <class T>
+    concept rgb_interface = requires(T a, RGB pixel) {
+        a.put_pixel(pixel);
+    };
+
+    struct WRGB
+    {
+        uint8_t white;
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+    };
+    template <class T>
+    concept wrgb_interface = requires(T a, WRGB pixel) {
+        a.put_pixel(pixel);
+    };
+
+    class PIO_NeoPixel_Driver final
     {
     public:
-        using RGB_String_Driver::Callable_T;
-
-        explicit WRGB_String_Driver( PIO pio_index, int state_machine_index, int gpio_pin, size_t number_of_pixels ) noexcept :
-            RGB_String_Driver( pio_index, state_machine_index, gpio_pin, number_of_pixels )
+        explicit PIO_NeoPixel_Driver(PIO pio_index, int state_machine_index, int gpio_pin) noexcept : m_pio{pio_index}, m_state_machine{state_machine_index}, m_pin{gpio_pin}
         {
-            uint offset = pio_add_program( m_pio, &ws2812_program );
-            ws2812_program_init( m_pio, m_state_machine, offset, gpio_pin, 800000, true );
         }
 
-        void put_pixel( uint8_t w, uint8_t r, uint8_t g, uint8_t b )
+        void set_rgb_mode() noexcept
         {
-            pio_sm_put_blocking( m_pio, m_state_machine, wrgb_u32( w, r, g, b ) );
+            uint offset = pio_add_program(m_pio, &ws2812_program);
+            ws2812_program_init(m_pio, m_state_machine, offset, m_pin, 800000, false);
         }
-        void put_pixel( uint8_t w )
+        void set_wrgb_mode() noexcept
         {
-            pio_sm_put_blocking( m_pio, m_state_machine, wrgb_u32( w, 0, 0, 0 ) );
+            uint offset = pio_add_program(m_pio, &ws2812_program);
+            ws2812_program_init(m_pio, m_state_machine, offset, m_pin, 800000, false);
+        }
+
+        void put_pixel(uint32_t pixel_value) noexcept
+        {
+            pio_sm_put_blocking(m_pio, m_state_machine, pixel_value);
         }
 
     private:
-        inline uint32_t wrgb_u32( uint8_t w, uint8_t r, uint8_t g, uint8_t b )
+        PIO m_pio;
+        int m_state_machine;
+        int m_pin;
+    };
+
+    template <string_interface Driver>
+    class RGB_Driver final
+    {
+    public:
+        constexpr explicit RGB_Driver(Driver &drv) noexcept : m_drv{drv}
         {
-            return ( static_cast< uint32_t >( r ) << 24 ) |
-                   ( static_cast< uint32_t >( g ) << 16 ) |
-                   ( static_cast< uint32_t >( b ) << 8 ) |
-                   static_cast< uint32_t >( w );
+            m_drv.set_rgb_mode();
         }
+
+        constexpr void put_pixel(RGB pixel) noexcept
+        {
+            m_drv.put_pixel(rgb_u32(pixel));
+        }
+
+    protected:
+        Driver &m_drv;
+
+        [[nodiscard]] static constexpr uint32_t rgb_u32(RGB pixel) noexcept
+        {
+            return (static_cast<uint32_t>(pixel.red) << 16) |
+                   (static_cast<uint32_t>(pixel.green) << 24) |
+                   (static_cast<uint32_t>(pixel.blue) << 8);
+        }
+    };
+
+    template <string_interface Driver>
+    class WRGB_Driver final
+    {
+    public:
+        constexpr explicit WRGB_Driver(Driver &drv) noexcept : m_drv{drv}
+        {
+            m_drv.set_wrgb_mode();
+        }
+
+        constexpr void put_pixel(WRGB pixel) noexcept
+        {
+            m_drv.put_pixel(wrgb_u32(pixel));
+        }
+        constexpr void put_pixel(uint8_t w) noexcept
+        {
+            m_drv.put_pixel(wrgb_u32(WRGB{.white{w}, .red{0}, .green{0}, .blue{0}}));
+        }
+
+    private:
+        Driver &m_drv;
+
+        [[nodiscard]] static constexpr uint32_t wrgb_u32(WRGB pixel) noexcept
+        {
+            return (static_cast<uint32_t>(pixel.red) << 24) |
+                   (static_cast<uint32_t>(pixel.green) << 16) |
+                   (static_cast<uint32_t>(pixel.blue) << 8) |
+                   static_cast<uint32_t>(pixel.white);
+        }
+    };
+
+    template <rgb_interface Driver>
+    class RGB_Pattern_Driver final
+    {
+    public:
+        RGB_Pattern_Driver(Driver &drv, uint number_of_pixels) noexcept : m_drv{drv}, m_number_of_pixels{number_of_pixels} {}
+
+        template <class Callable>
+        constexpr void put_pattern(Callable pattern_generator) noexcept
+        {
+            for (uint ii = 0; ii < m_number_of_pixels; ++ii)
+            {
+                m_drv.put_pixel(pattern_generator(ii));
+            }
+        }
+
+    private:
+        Driver &m_drv;
+        uint m_number_of_pixels;
+    };
+
+    template <wrgb_interface Driver>
+    class WRGB_Pattern_Driver final
+    {
+    public:
+        WRGB_Pattern_Driver(Driver &drv, uint number_of_pixels) noexcept : m_drv{drv}, m_number_of_pixels{number_of_pixels} {}
+
+        template <class Callable>
+        constexpr void put_pattern(Callable pattern_generator) noexcept
+        {
+            for (uint ii = 0; ii < m_number_of_pixels; ++ii)
+            {
+                m_drv.put_pixel(pattern_generator(ii));
+            }
+        }
+
+    private:
+        Driver &m_drv;
+        uint m_number_of_pixels;
     };
 }
 
